c71e6440909c3e26545c7ab3528c844a4b24802a
DDDanny <github@falss.de>
2017-09-29 23:07:40 +0200

    goal: retrieve all gets from a given devicelist or pass to the appropiate fritzbox call --- preparation: renamed the current getSwitchList to getOuletList added alarm bitmask --- new: bitmask based getListByFunction with caching via options.devicelist getSwitchList calls getListByFunction which will us cache or retrieve data liek before via getOuletList new: 3 functions to retrieve getAlarmList, getTemperatureSensorsList, getEnergyMeterList getValveList introduced to have a common english in contrast to ...thermostat... --- changed the documentation and added the new functions --- replaced duplicate coding with internal verify Session call

--- index.js
+++ index.js
@@ -433,7 +433,7 @@ module.exports.getListByFunction = function(sid, options, bit)
             return device.identifier.replace(/\s/g, ''); // fix ain, no gaps
         });
 
-        return devices;
+        Promise.resolve(return devices);
     });
 };
 
