a35bbf025ef2ee7a9ec07ff911202c2c8055dfbb
DDDanny <github@falss.de>
2017-10-01 23:21:44 +0200

    caching stable, preparing tests

---
+++ test/test-getListByFunctions.spec.js
@@ -0,0 +1,14 @@
+//testing of getListByFunctions 
+const chai = require('chai');
+
+describe('getListByFunction', function () {
+    it('undefined bit => empty list', function * () {
+        fritzapi.getListByFunction(undefined, undefined, undefined).should.eventually.be.an('array').should.eventually.be.empty;
+    })
+    
+    it('0 bits => empty list', function * () {
+        fritzapi.getListByFunction(undefined, 0, undefined).then(function(devicelist) {
+            devicelist.should.be.an('array').that.should.be.empty;
+        });
+    })
+})
